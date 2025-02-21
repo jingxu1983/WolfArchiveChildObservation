a716fddebb1a78cc2b7cb68004a3a5fa22ed9536
jingxu1983 <jingxu1983@gmail.com>
2025-02-20 20:43:47 -0800

    Add files via upload

---
+++ CO_LDA_topic_modeling.py
@@ -0,0 +1,593 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install -U spacy
+
+
+# In[2]:
+
+
+import nltk; nltk.download('stopwords')
+
+
+# In[3]:
+
+
+import re
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+# spacy for lemmatization
+import spacy
+
+# Plotting tools
+import pyLDAvis
+import pyLDAvis.gensim  # don't skip this
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Enable logging for gensim - optional
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+
+
+# In[4]:
+
+
+# NLTK Stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+
+
+# In[5]:
+
+
+lemma_text = pd.read_excel('ChildObservationLemmaDataNoNumNoP_0622.xlsx')
+lemma_text.head()
+
+
+# In[6]:
+
+
+text = lemma_text['Lemmatize']
+text[350]
+
+
+# In[8]:
+
+
+## Tokenize text
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+data_words = list(sent_to_words(text))
+
+print(data_words[:1])
+
+
+# ## Creating Bigram and Trigram Models
+
+# In[9]:
+
+
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+
+# Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# See trigram example
+print(trigram_mod[bigram_mod[data_words[0]]])
+
+
+# ## Remove Stopwords, Make Bigrams and Lemmatize
+
+# In[10]:
+
+
+# Define functions for stopwords, bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+# In[16]:
+
+
+# Remove Stop Words
+data_words_nostops = remove_stopwords(data_words)
+
+# Form Bigrams
+data_words_bigrams = make_bigrams(data_words_nostops)
+
+# Initialize spacy 'en' model, keeping only tagger component (for efficiency)
+# python3 -m spacy download en
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+# Do lemmatization keeping only noun, adj, vb, adv
+data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+
+print(data_lemmatized[:1])
+
+
+# ## Create the Dictionary and Corpus needed for Topic Modeling
+
+# In[17]:
+
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_lemmatized)
+
+# Create Corpus
+texts = data_lemmatized
+
+# Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]
+
+# View
+print(corpus[:1])
+
+
+# In[18]:
+
+
+id2word[0]
+
+
+# In[19]:
+
+
+# Human readable format of corpus (term-frequency)
+[[(id2word[id], freq) for id, freq in cp] for cp in corpus[:1]]
+
+
+# In[20]:
+
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=20, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=10,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+
+# In[21]:
+
+
+# Print the Keyword in the 20 topics
+pprint(lda_model.print_topics())
+doc_lda = lda_model[corpus]
+
+
+# ## Compute Model Perplexity and Coherence Score
+
+# In[ ]:
+
+
+# Compute Perplexity
+print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
+
+
+# ## Building LDA Mallet Model
+
+# In[29]:
+
+
+import os
+os.listdir()
+
+
+# In[32]:
+
+
+os.path.abspath("mallet-2.0.8/bin/mallet")
+
+
+# In[34]:
+
+
+# Download File: http://mallet.cs.umass.edu/dist/mallet-2.0.8.zip
+mallet_path = '/Users/jing/Documents/MyDocuments/CurrentProjects/WolfProject/DataOrganizationAnalyses/ChildObservation/TextMining/TopicModeling/Unsupervised_New_August2020/mallet-2.0.8/bin/mallet' 
+ldamallet = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=20, id2word=id2word)
+
+
+# In[35]:
+
+
+# Show Topics
+pprint(ldamallet.show_topics(formatted=False))
+
+# Compute Coherence Score
+coherence_model_ldamallet = CoherenceModel(model=ldamallet, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_ldamallet = coherence_model_ldamallet.get_coherence()
+print('\nCoherence Score: ', coherence_ldamallet)
+
+
+# ## How to find the optimal number of topics for LDA?
+
+# In[36]:
+
+
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=num_topics, id2word=id2word)
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+
+# In[37]:
+
+
+# Can take a long time to run.
+model_list, coherence_values = compute_coherence_values(dictionary=id2word, corpus=corpus, texts=data_lemmatized, start=2, limit=40, step=6)
+
+
+# In[38]:
+
+
+# Show graph
+limit=40; start=2; step=6;
+x = range(start, limit, step)
+plt.plot(x, coherence_values)
+plt.xlabel("Num Topics")
+plt.ylabel("Coherence score")
+plt.legend(("coherence_values"), loc='best')
+plt.show()
+
+
+# In[39]:
+
+
+# Print the coherence scores, highest score: 14 topics
+for m, cv in zip(x, coherence_values):
+    print("Num Topics =", m, " has Coherence Value of", round(cv, 4))
+
+
+# In[45]:
+
+
+# Choose the best LDA model
+lda_model_optimal = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=14, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=10,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+
+# In[48]:
+
+
+# Print the Keyword in the 14 topics
+pprint(lda_model_optimal.print_topics())
+doc_lda = lda_model_optimal[corpus]
+
+
+# In[47]:
+
+
+# Compute Perplexity
+print('\nPerplexity: ', lda_model_optimal.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
+
+# Compute Coherence Score
+coherence_model_lda_optimal = CoherenceModel(model=lda_model_optimal, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_lda_optimal = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda_optimal)
+
+
+# In[83]:
+
+
+optimal_model = model_list[2]
+model_topics = optimal_model.show_topics(formatted=False)
+pprint(optimal_model.print_topics(num_words=15))
+
+
+# ## Finding the dominant topic in each doc
+
+# In[74]:
+
+
+def format_topics_sentences(ldamodel=optimal_model, corpus=corpus, texts=text):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row in enumerate(ldamodel[corpus]):
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=optimal_model, corpus=corpus, texts=text)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+
+# Show
+df_dominant_topic.head(10)
+
+
+# In[75]:
+
+
+df_dominant_topic.to_excel(r'UnsupervisedTopicModeling_Gensim_Optimal_Keywords.xlsx', index = False)
+
+
+# ## Find the most representative document for each topic
+
+# In[76]:
+
+
+# Group top 5 sentences under each topic
+sent_topics_sorteddf_mallet = pd.DataFrame()
+
+sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+for i, grp in sent_topics_outdf_grpd:
+    sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet, 
+                                             grp.sort_values(['Perc_Contribution'], ascending=[0]).head(1)], 
+                                            axis=0)
+
+# Reset Index    
+sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+# Format
+sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Text"]
+
+# Show
+sent_topics_sorteddf_mallet.head()
+
+
+# In[77]:
+
+
+sent_topics_sorteddf_mallet.to_excel(r'UnsupervisedTopicModeling_Gensim_Optimal_RepresentativeDocs.xlsx', index = False)
+
+
+# ## Topic distribution across documents
+
+# In[78]:
+
+
+# Number of Documents for Each Topic
+topic_counts = df_topic_sents_keywords['Dominant_Topic'].value_counts()
+
+df_topic_distribution = topic_counts.reset_index(name="Num Documents")
+df_topic_distribution.columns = ['Topic Num', 'Num Documents']
+df_topic_distribution
+
+# Show
+df_topic_distribution.head()
+
+
+# In[79]:
+
+
+df_topic_distribution.to_excel(r'UnsupervisedTopicModeling_Gensim_Optimal_TopicNumDocs.xlsx', index = False)
+
+
+# In[88]:
+
+
+# Visualize the topics
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda_model_optimal, corpus, id2word)
+vis
+
+
+# In[89]:
+
+
+pyLDAvis.save_html(vis, 'LDA_Gensim_Optimal_Visualization/lda.html')
+
+
+# In[103]:
+
+
+## Frequency Distribution of Word Counts in Documents
+
+## When working with a large number of documents, you want to know how big the documents are as a whole and by topic. Let’s plot the document word counts distribution.
+
+doc_lens = df_dominant_topic['Text'].str.split().str.len()
+
+# Plot
+plt.figure(figsize=(16,7), dpi=160)
+plt.hist(doc_lens, bins = 1000, color='navy')
+plt.text(750, 100, "Mean   : " + str(round(np.mean(doc_lens))))
+plt.text(750,  90, "Median : " + str(round(np.median(doc_lens))))
+plt.text(750,  80, "Stdev   : " + str(round(np.std(doc_lens))))
+plt.text(750,  70, "1%ile    : " + str(round(np.quantile(doc_lens, q=0.01))))
+plt.text(750,  60, "99%ile  : " + str(round(np.quantile(doc_lens, q=0.99))))
+
+plt.gca().set(xlim=(0, 500), ylabel='Number of Documents', xlabel='Document Word Count')
+plt.tick_params(size=16)
+plt.xticks(np.linspace(0,500,9))
+plt.title('Distribution of Document Word Counts', fontdict=dict(size=22))
+plt.savefig('doc_word_count.png')
+plt.show()
+
+
+# In[109]:
+
+
+import seaborn as sns
+import matplotlib.colors as mcolors
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+fig, axes = plt.subplots(7,2,figsize=(16,14), dpi=160, sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):    
+    df_dominant_topic_sub = df_dominant_topic.loc[df_dominant_topic.Dominant_Topic == i, :]
+    doc_lens = df_dominant_topic_sub['Text'].str.split().str.len()
+    ax.hist(doc_lens, bins = 1000, color=cols[i])
+    ax.tick_params(axis='y', labelcolor=cols[i], color=cols[i])
+    sns.kdeplot(doc_lens, color="black", shade=False, ax=ax.twinx())
+    ax.set(xlim=(0, 1000), xlabel='Document Word Count')
+    ax.set_ylabel('Number of Documents', color=cols[i])
+    ax.set_title('Topic: '+str(i), fontdict=dict(size=16, color=cols[i]))
+
+fig.tight_layout()
+fig.subplots_adjust(top=0.90)
+plt.xticks(np.linspace(0,500,9))
+fig.suptitle('Distribution of Document Word Counts by Dominant Topic', fontsize=22)
+plt.savefig("doc_word_count_by_dominant_topic.png")
+plt.show()
+
+
+# ## Word Clouds of Top N Keywords in Each Topic
+
+# In[110]:
+
+
+# 1. Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=stop_words,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(5, 2, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
+
+
+# In[107]:
+
+
+from collections import Counter
+topics = lda_model.show_topics(formatted=False)
+data_flat = [w for w_list in data_lemmatized for w in w_list]
+counter = Counter(data_flat)
+
+out = []
+for i, topic in topics:
+    for word, weight in topic:
+        out.append([word, i , weight, counter[word]])
+
+df = pd.DataFrame(out, columns=['word', 'topic_id', 'importance', 'word_count'])        
+
+# Plot Word Count and Weights of Topic Keywords
+fig, axes = plt.subplots(7, 2, figsize=(16,10), sharey=True, dpi=160)
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+for i, ax in enumerate(axes.flatten()):
+    ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
+    ax_twin = ax.twinx()
+    ax_twin.bar(x='word', height="importance", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.2, label='Weights')
+    ax.set_ylabel('Word Count', color=cols[i])
+    ax_twin.set_ylim(0, 0.030); ax.set_ylim(0, 3500)
+    ax.set_title('Topic: ' + str(i), color=cols[i], fontsize=16)
+    ax.tick_params(axis='y', left=False)
+    ax.set_xticklabels(df.loc[df.topic_id==i, 'word'], rotation=30, horizontalalignment= 'right')
+    ax.legend(loc='upper left'); ax_twin.legend(loc='upper right')
+
+fig.tight_layout(w_pad=2)    
+fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=22, y=1.05)    
+plt.show()
+
+
+# In[ ]:
+
+
+
+
---
+++ CO_subset_GPT.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+### Install Libraries ###
+get_ipython().system('pip install --upgrade pip')
+get_ipython().system('pip install openai pandas matplotlib tiktoken')
+
+
+# In[3]:
+
+
+### Import Libraries ###
+# Import the openai library for accessing OpenAI's API functionalities
+import openai
+# Import the OpenAI class from the openai library for more specific API interactions
+from openai import OpenAI
+# Import the os library to interact with the operating system, like reading or writing files
+import os
+# Import pandas, a powerful data manipulation and analysis library, as 'pd'
+import pandas as pd
+# Import matplotlib's pyplot to create static, interactive, and animated visualizations in Python, as 'plt'
+import matplotlib.pyplot as plt
+# Import the constellate library for working with datasets and analytics
+# Import time
+import time
+# Import tiktoken which helps us count tokens
+import tiktoken
+
+
+# In[11]:
+
+
+print(os.getcwd())
+
+
+# In[12]:
+
+
+### Import my dataset
+df = pd.read_csv("data/SBERT_Sample.csv")
+print(df.head())
+
+
+# In[14]:
+
+
+###Configure the OpenAI client
+
+#To setup the client for our use, we need to create an API key to use with our request. 
+## Method 1: Directly paste the API key (not recommended for production or shared code)
+client = OpenAI(api_key= "enter your own API key")
+
+
+# In[15]:
+
+
+# Define a function `get_completion` that takes a prompt and optionally a model name (defaulting to "gpt-3.5-turbo")
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    # Create a list of messages where each message is a dictionary with a role (user/system) and the content (the prompt)
+    messages = [{"role": "user", "content": prompt}]
+    # Call the OpenAI API's chat.completions.create method with the specified model, messages, and a temperature of 0
+    # Temperature of 0 makes the model's responses deterministic (the same input will always produce the same output), max 2.
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    # Return the content of the first message in the response's choices. This is the model's completion of the input prompt.
+    return response.choices[0].message.content
+
+# Test the function by defining a test prompt asking to explain what a large language model is in one sentence
+test_prompt = "Explain children's play in two sentence."
+# Print the result of calling `get_completion` with the test prompt to see the model's response
+print(get_completion(test_prompt))
+
+
+# In[17]:
+
+
+### LLM performance
+# Define a function to calculate the number of tokens in a string using a specific encoding
+def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
+    # Retrieve the encoding object for the specified encoding name using tiktoken library
+    encoding = tiktoken.get_encoding(encoding_name)
+    # Encode the string and calculate the number of tokens in the encoded result
+    num_tokens = len(encoding.encode(string))
+    # Return the number of tokens
+    return num_tokens
+
+# Define a function to analyze a text in parts, each part having a maximum number of tokens
+def analyze_text_in_parts(text, task, max_tokens=8000):
+    # Check if the input text is a list (of strings) or a single string
+    if isinstance(text, list):
+        # If it's a list, join the elements into a single string separated by spaces
+        text = ' '.join(text)
+    
+    # Initialize an empty list to hold the parts of text
+    parts = []
+    # Initialize an empty string to accumulate the current part of text
+    current_part = ""
+    # Split the text into words
+    words = text.split()
+    
+    # Iterate over each word in the text
+    for word in words:
+        # Check if adding the current word to the current part exceeds the max_tokens limit
+        if num_tokens_from_string(current_part + " " + word) > max_tokens:
+            # If so, add the current part to the parts list and start a new part with the current word
+            parts.append(current_part)
+            current_part = word
+        else:
+            # Otherwise, add the current word to the current part
+            current_part += " " + word
+    
+    # After iterating through all words, add the last part to the parts list if it's not empty
+    if current_part:
+        parts.append(current_part)
+    
+    # Initialize an empty string to accumulate the aggregated response from analyzing each part
+    aggregated_response = ""
+    # Iterate over each part
+    for part in parts:
+        # Construct a prompt for the analysis task using the current part
+        prompt = f"Analyze the following text for {task}: {part}"
+        # Get the response for the prompt using the get_completion function
+        response = get_completion(prompt)
+        # Append the response to the aggregated response, separated by spaces
+        aggregated_response += response + " "
+        # Sleep for 1 second to avoid hitting rate limits of the API
+        time.sleep(1)
+    
+    # Return the aggregated response, stripped of leading/trailing whitespace
+    return aggregated_response.strip()
+
+# Define a list of tasks for analysis (can replace the following tasks with "sentiment analysis", "topic models", etc.)
+tasks = ["sentiment", "topic models"]
+# Initialize an empty list to store the results
+results = []
+
+# Iterate over each row in the dataframe `df`
+for _, row in df.iterrows():
+    # For each task, analyze the full text of the row
+    for task in tasks:
+        analysis = analyze_text_in_parts(row['Content'], task)
+        # Append the analysis result to the results list
+        results.append({"Text": row['ObsIndex'], "Task": task, "Analysis": analysis})
+    
+    # After analyzing all tasks for a row, save the results to a CSV file to prevent data loss
+    pd.DataFrame(results).to_csv('ChildPlay1_analysis.csv', index=False)
+
+# Convert the results list to a DataFrame and print it
+df_results = pd.DataFrame(results)
+print(df_results)
+
+
+# In[22]:
+
+
+### LLM performance
+# Define a function to calculate the number of tokens in a string using a specific encoding
+def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
+    # Retrieve the encoding object for the specified encoding name using tiktoken library
+    encoding = tiktoken.get_encoding(encoding_name)
+    # Encode the string and calculate the number of tokens in the encoded result
+    num_tokens = len(encoding.encode(string))
+    # Return the number of tokens
+    return num_tokens
+
+# Define a function to analyze a text in parts, each part having a maximum number of tokens
+def analyze_text_in_parts(text, task, max_tokens=8000):
+    # Check if the input text is a list (of strings) or a single string
+    if isinstance(text, list):
+        # If it's a list, join the elements into a single string separated by spaces
+        text = ' '.join(text)
+    
+    # Initialize an empty list to hold the parts of text
+    parts = []
+    # Initialize an empty string to accumulate the current part of text
+    current_part = ""
+    # Split the text into words
+    words = text.split()
+    
+    # Iterate over each word in the text
+    for word in words:
+        # Check if adding the current word to the current part exceeds the max_tokens limit
+        if num_tokens_from_string(current_part + " " + word) > max_tokens:
+            # If so, add the current part to the parts list and start a new part with the current word
+            parts.append(current_part)
+            current_part = word
+        else:
+            # Otherwise, add the current word to the current part
+            current_part += " " + word
+    
+    # After iterating through all words, add the last part to the parts list if it's not empty
+    if current_part:
+        parts.append(current_part)
+    
+    # Initialize an empty string to accumulate the aggregated response from analyzing each part
+    aggregated_response = ""
+    # Iterate over each part
+    for part in parts:
+        # Construct a prompt for the analysis task using the current part
+        prompt = f"Analyze the following text for {task}: {part}"
+        # Get the response for the prompt using the get_completion function
+        response = get_completion(prompt)
+        # Append the response to the aggregated response, separated by spaces
+        aggregated_response += response + " "
+        # Sleep for 1 second to avoid hitting rate limits of the API
+        time.sleep(1)
+    
+    # Return the aggregated response, stripped of leading/trailing whitespace
+    return aggregated_response.strip()
+
+# Define a list of tasks for analysis (can replace the following tasks with "sentiment analysis", "topic models", etc.)
+tasks_new = ["classification as cooperation in score 0-10", "classification as conflict in score 0-10", "classification as pretend-play in score 0-10"]
+# Initialize an empty list to store the results
+results_new = []
+
+# Iterate over each row in the dataframe `df`
+for _, row in df.iterrows():
+    # For each task, analyze the full text of the row
+    for task_new in tasks_new:
+        analysis = analyze_text_in_parts(row['Content'], task_new)
+        # Append the analysis result to the results list
+        results_new.append({"Text": row['ObsIndex'], "Task": task_new, "Analysis": analysis})
+    
+    # After analyzing all tasks for a row, save the results to a CSV file to prevent data loss
+    pd.DataFrame(results_new).to_csv('ChildPlay1_analysis_new.csv', index=False)
+
+# Convert the results list to a DataFrame and print it
+df_results_new = pd.DataFrame(results_new)
+print(df_results_new)
+
+
+# In[24]:
+
+
+# concatenate strings
+def concat_strings(values):
+    return ', '.join(values)
+
+
+# In[33]:
+
+
+#make long tables wide
+df1 = df_results.pivot_table(index='Text', columns='Task', values='Analysis', aggfunc=concat_strings)
+print(df1.head())
+pd.DataFrame(df1).to_csv('ChildPlay1_df1.csv', index=True)
+
+
+# In[34]:
+
+
+#make long tables wide
+df2 = df_results_new.pivot_table(index='Text', columns='Task', values='Analysis', aggfunc=concat_strings)
+print(df2.head())
+pd.DataFrame(df2).to_csv('ChildPlay1_df2.csv', index=True)
+
+
+# In[35]:
+
+
+# Extract a column from the first DataFrame
+merged_df = df2.merge(df1, on='Text', how = 'outer')
+print(merged_df.head())
+pd.DataFrame(merged_df).to_csv('ChildPlay1.csv', index=True)
+
+
+# In[36]:
+
+
+pd.DataFrame(merged_df).to_excel('ChildPlay1.xlsx', index=True)
+
+
+
+
+
+
